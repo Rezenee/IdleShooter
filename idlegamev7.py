737d20a9a3b7d483f264317211436aef46a169ef
ZaneRickert <45903462+Rezenee@users.noreply.github.com>
2019-03-08 12:14:17 -0500

    Add system to change keybinds much improved

--- idlegamev7.py
+++ idlegamev7.py
@@ -5,6 +5,7 @@ import time
 import random
 import math
 import os
+from pygame.locals import *
 pygame.init()
 
 xres = 1024
@@ -53,49 +54,52 @@ gear = pygame.image.load(os.path.join('images', 'gear.png'))
 weaponFlash = pygame.image.load(os.path.join('images', 'weaponflash.png'))
 clock = pygame.time.Clock()
 gameDisplay.fill(gray)
-pygame.mixer.music.load(os.path.join('images', 'background.wav'))
+pygame.mixer.music.load(os.path.join('images', 'background.mp3'))
 font = pygame.font.SysFont("Verdana", 12)
 hitmarkers = []
-keyNumValue = [pygame.K_BACKSPACE, pygame.K_TAB, pygame.K_CLEAR, pygame.K_RETURN, pygame.K_PAUSE, pygame.K_ESCAPE,
-               pygame.K_SPACE, pygame.K_EXCLAIM, pygame.K_QUOTEDBL, pygame.K_HASH, pygame.K_DOLLAR, pygame.K_AMPERSAND,
-               pygame.K_QUOTE, pygame.K_LEFTPAREN,pygame.K_RIGHTPAREN, pygame.K_ASTERISK, pygame.K_PLUS, pygame.K_COMMA,
-               pygame.K_MINUS, pygame.K_PERIOD, pygame.K_SLASH,pygame.K_0, pygame.K_1, pygame.K_2, pygame.K_3,
-               pygame.K_4,pygame.K_5, pygame.K_6, pygame.K_7, pygame.K_8, pygame.K_9,pygame.K_COLON, pygame.K_SEMICOLON,
-               pygame.K_LESS, pygame.K_EQUALS, pygame.K_GREATER, pygame.K_QUESTION, pygame.K_AT,pygame.K_LEFTBRACKET,
-               pygame.K_BACKSLASH, pygame.K_RIGHTBRACKET, pygame.K_CARET, pygame.K_UNDERSCORE, pygame.K_BACKQUOTE,
-               pygame.K_DELETE, pygame.K_KP0, pygame.K_KP1, pygame.K_KP2, pygame.K_KP3, pygame.K_KP4, pygame.K_KP5,
-               pygame.K_KP6, pygame.K_KP7,pygame.K_KP8, pygame.K_KP9, pygame.K_KP_PERIOD, pygame.K_KP_DIVIDE,
-               pygame.K_KP_MULTIPLY, pygame.K_KP_MINUS, pygame.K_KP_PLUS,pygame.K_KP_ENTER, pygame.K_KP_EQUALS,
-               pygame.K_UP, pygame.K_DOWN, pygame.K_RIGHT, pygame.K_LEFT, pygame.K_INSERT,pygame.K_HOME, pygame.K_END,
-               pygame.K_PAGEUP, pygame.K_PAGEDOWN, pygame.K_F1, pygame.K_F2, pygame.K_F3, pygame.K_F4,pygame.K_F5,
-               pygame.K_F6, pygame.K_F7, pygame.K_F8, pygame.K_F9, pygame.K_F10, pygame.K_F11, pygame.K_F12,
-               pygame.K_F13,pygame.K_F14, pygame.K_F15, pygame.K_NUMLOCK, pygame.K_CAPSLOCK, pygame.K_SCROLLOCK,
-               pygame.K_RSHIFT, pygame.K_LSHIFT,pygame.K_RCTRL, pygame.K_LCTRL, pygame.K_RALT, pygame.K_LALT,
-               pygame.K_RMETA, pygame.K_LMETA, pygame.K_LSUPER,pygame.K_RSUPER, pygame.K_MODE, pygame.K_HELP,
-               pygame.K_PRINT, pygame.K_SYSREQ, pygame.K_BREAK, pygame.K_MENU,pygame.K_POWER, pygame.K_EURO, pygame.K_a,
-               pygame.K_b, pygame.K_c, pygame.K_d, pygame.K_e, pygame.K_f, pygame.K_g,pygame.K_h, pygame.K_i,pygame.K_j,
-               pygame.K_k, pygame.K_l, pygame.K_m, pygame.K_n, pygame.K_o, pygame.K_p,pygame.K_q, pygame.K_r,pygame.K_s,
-               pygame.K_t, pygame.K_u, pygame.K_v, pygame.K_w, pygame.K_x, pygame.K_y, pygame.K_z]
-
-for x in keyNumValue:
-    print(x)
+# keyNumValue = [pygame.K_BACKSPACE, pygame.K_TAB, pygame.K_CLEAR, pygame.K_RETURN, pygame.K_PAUSE, pygame.K_ESCAPE,
+#                pygame.K_SPACE, pygame.K_EXCLAIM, pygame.K_QUOTEDBL, pygame.K_HASH, pygame.K_DOLLAR, pygame.K_AMPERSAND,
+#                pygame.K_QUOTE, pygame.K_LEFTPAREN,pygame.K_RIGHTPAREN, pygame.K_ASTERISK, pygame.K_PLUS, pygame.K_COMMA,
+#                pygame.K_MINUS, pygame.K_PERIOD, pygame.K_SLASH,pygame.K_0, pygame.K_1, pygame.K_2, pygame.K_3,
+#                pygame.K_4,pygame.K_5, pygame.K_6, pygame.K_7, pygame.K_8, pygame.K_9,pygame.K_COLON, pygame.K_SEMICOLON,
+#                pygame.K_LESS, pygame.K_EQUALS, pygame.K_GREATER, pygame.K_QUESTION, pygame.K_AT,pygame.K_LEFTBRACKET,
+#                pygame.K_BACKSLASH, pygame.K_RIGHTBRACKET, pygame.K_CARET, pygame.K_UNDERSCORE, pygame.K_BACKQUOTE,
+#                pygame.K_DELETE, pygame.K_KP0, pygame.K_KP1, pygame.K_KP2, pygame.K_KP3, pygame.K_KP4, pygame.K_KP5,
+#                pygame.K_KP6, pygame.K_KP7,pygame.K_KP8, pygame.K_KP9, pygame.K_KP_PERIOD, pygame.K_KP_DIVIDE,
+#                pygame.K_KP_MULTIPLY, pygame.K_KP_MINUS, pygame.K_KP_PLUS,pygame.K_KP_ENTER, pygame.K_KP_EQUALS,
+#                pygame.K_UP, pygame.K_DOWN, pygame.K_RIGHT, pygame.K_LEFT, pygame.K_INSERT,pygame.K_HOME, pygame.K_END,
+#                pygame.K_PAGEUP, pygame.K_PAGEDOWN, pygame.K_F1, pygame.K_F2, pygame.K_F3, pygame.K_F4,pygame.K_F5,
+#                pygame.K_F6, pygame.K_F7, pygame.K_F8, pygame.K_F9, pygame.K_F10, pygame.K_F11, pygame.K_F12,
+#                pygame.K_F13,pygame.K_F14, pygame.K_F15, pygame.K_NUMLOCK, pygame.K_CAPSLOCK, pygame.K_SCROLLOCK,
+#                pygame.K_RSHIFT, pygame.K_LSHIFT,pygame.K_RCTRL, pygame.K_LCTRL, pygame.K_RALT, pygame.K_LALT,
+#                pygame.K_RMETA, pygame.K_LMETA, pygame.K_LSUPER,pygame.K_RSUPER, pygame.K_MODE, pygame.K_HELP,
+#                pygame.K_PRINT, pygame.K_SYSREQ, pygame.K_BREAK, pygame.K_MENU,pygame.K_POWER, pygame.K_EURO, pygame.K_a,
+#                pygame.K_b, pygame.K_c, pygame.K_d, pygame.K_e, pygame.K_f, pygame.K_g,pygame.K_h, pygame.K_i,pygame.K_j,
+#                pygame.K_k, pygame.K_l, pygame.K_m, pygame.K_n, pygame.K_o, pygame.K_p,pygame.K_q, pygame.K_r,pygame.K_s,
+#                pygame.K_t, pygame.K_u, pygame.K_v, pygame.K_w, pygame.K_x, pygame.K_y, pygame.K_z]
 
 reloadKey = 112
+def get_key():
+  while 1:
+    event = pygame.event.poll()
+    if event.type == KEYDOWN:
+      return event.key
+    else:
+      pass
+
+
 def changeKeyBind():
     global reloadKey
     pressed = pygame.key.get_pressed()
     check = 1
 
-    for x in keyNumValue:
-        if pressed[x]:
-            reloadKey = x
-            check = 0
-
-    # for x in keyNumValue:
-    #     if pressed[x]:
-    #         reloadKey = x
-    #         time.sleep(1)
-    print(reloadKey)
+    while 1:
+        inkey = get_key()
+        if inkey == K_RETURN:
+             break
+        elif inkey <= 127:
+            reloadKey = inkey
+            print(reloadKey)
 
 class gun(object):
     def __init__(self, cost, gunSelectIdle, gunSelectPrac, gunBought):
@@ -561,7 +565,7 @@ def keyBindScreen():
         gameDisplay.fill(gray)
         fontsmall.render_to(gameDisplay, (80, 70), "Keybinds", (black))
         button("Back", 100, 650, 100, 50, green, bright_green)
-        button("Reload", 100,200, 100,50, green,bright_green,changeKeyBind)
+        button(("Reload: " + chr(reloadKey)), 100,200, 100,50, green,bright_green,changeKeyBind)
         pygame.display.update()
         clock.tick(60)
 
